a99f020d4f82f47bacc0728565c25e8e1f4d4558
qingyu <166664660@qq.com>
2023-10-26 13:49:28 +0800

    Update PreLoader.cpp

--- src/pl/PreLoader.cpp
+++ src/pl/PreLoader.cpp
@@ -22,74 +22,7 @@ std::set<std::string> preloadList;
 
 namespace pl {
 
-void addLibraryToPath() {
-    auto* buffer = new (std::nothrow) WCHAR[MAX_PATH_LENGTH];
-    if (!buffer) return;
-
-    DWORD        length = GetEnvironmentVariableW(L"PATH", buffer, MAX_PATH_LENGTH);
-    std::wstring path(buffer, length);
-    length = GetCurrentDirectoryW(MAX_PATH_LENGTH, buffer);
-    std::wstring currentDir(buffer, length);
-
-    SetEnvironmentVariableW(L"PATH", (currentDir + L"\\plugins\\lib;" + path).c_str());
-    delete[] buffer;
-}
-
-
-bool loadLibrary(const string& libName, bool showFailInfo = true) {
-    if (LoadLibraryW(str2wstr(libName).c_str())) {
-        Info("{} Injected.", std::filesystem::path(libName).filename().u8string());
-        return true;
-    } else {
-        if (showFailInfo) {
-            DWORD error_message_id = GetLastError();
-            Error("Can't load {} !", libName);
-            Error("Error code: {} !", error_message_id);
-            LPWSTR message_buffer = nullptr;
-            FormatMessage(
-                FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_SYSTEM,
-                nullptr,
-                error_message_id,
-                NULL,
-                (LPWSTR)&message_buffer,
-                NULL,
-                nullptr
-            );
-            Error("{}", wstr2str(message_buffer));
-            LocalFree(message_buffer);
-        }
-        return false;
-    }
-}
-
-void loadRawLibraries() {
-    std::filesystem::create_directories("plugins");
-    directory_iterator ent("plugins");
-    for (auto& file : ent) {
-        if (!file.is_regular_file()) continue;
-
-        auto& path     = file.path();
-        auto  fileName = path.u8string();
-
-        string ext = path.extension().u8string();
-        if (ext != ".dll") { continue; }
-
-        if (preloadList.count(fileName)) continue;
-
-        string dllFileName = path.filename().u8string();
-        auto   lib         = LoadLibrary(str2wstr(fileName).c_str());
-        if (lib) {
-            Info("Dll <{}> Injected", dllFileName);
-        } else {
-            Error("Fail to load library [{}]", dllFileName);
-            Error("Error: Code[{}]", GetLastError());
-        }
-    }
-}
-
 void init() {
-    loadLoggerConfig();
-    addLibraryToPath();
     pl::symbol_provider::init();
 }
 } // namespace pl
