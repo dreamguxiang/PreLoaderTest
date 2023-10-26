5d60433098d2cd6fb102f160e1ddb84d9cc1c853
qingyu <166664660@qq.com>
2023-10-26 14:04:37 +0800

    Update PreLoader.cpp

--- src/pl/PreLoader.cpp
+++ src/pl/PreLoader.cpp
@@ -29,23 +29,6 @@ void init() {
 
 
 [[maybe_unused]] BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call != DLL_PROCESS_ATTACH) return TRUE;
-
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-
-    DWORD mode;
-    GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &mode);
-    SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-
-    // For #683, Change CWD to current module path
-    auto buffer = new wchar_t[MAX_PATH];
-    GetModuleFileNameW(hModule, buffer, MAX_PATH);
-    std::wstring path(buffer);
-    auto         cwd = path.substr(0, path.find_last_of('\\'));
-    SetCurrentDirectoryW(cwd.c_str());
-    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOALIGNMENTFAULTEXCEPT);
-
     pl::init();
     return TRUE;
 }
